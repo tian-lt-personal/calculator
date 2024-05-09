aefe0ed1c2397a0cf117e3689d83297dba667ed1
Tian L <tankle_@hotmail.com>
2024-05-09 22:23:12 +0900

    testing

--- src/RatpakMigrationTests/CreationTests.cpp
+++ src/RatpakMigrationTests/CreationTests.cpp
@@ -94,4 +94,39 @@ TEST(CreationTests, IntegralToNum)
     RunUInt32R9(std::numeric_limits<uint32_t>::max());
 }
 
+TEST(CreationTests, NumToIntegral)
+{
+    constexpr auto RunR10 = [](int value)
+    {
+        auto oldnum = i32tonum(value, 10);
+        auto num = ratpak::number<10>(value);
+        ASSERT_EQ(numtoi32(oldnum, 10), num.to_value<int32_t>());
+        destroynum(oldnum);
+    };
+    RunR10(0);
+    RunR10(10);
+    RunR10(10000);
+    RunR10(12345678);
+    RunR10(-1);
+    RunR10(-1000);
+    RunR10(-12345678);
+    RunR10(std::numeric_limits<int>::max());
+    RunR10(std::numeric_limits<int>::min() + 1);
 
+    constexpr auto RunR32 = [](int value)
+    {
+        auto oldnum = i32tonum(value, 32);
+        auto num = ratpak::number<32>(value);
+        ASSERT_EQ(numtoi32(oldnum, 32), num.to_value<int32_t>());
+        destroynum(oldnum);
+    };
+    RunR32(0);
+    RunR32(10);
+    RunR32(10000);
+    RunR32(12345678);
+    RunR32(-1);
+    RunR32(-1000);
+    RunR32(-12345678);
+    RunR32(std::numeric_limits<int>::max());
+    RunR32(std::numeric_limits<int>::min() + 1);
+}
